34d8a787c7bd98a8112a2c4e064b60ffa5d56591
Kilian <31303@ma-web.nl>
2020-09-18 12:28:21 +0200

    L2 opdracht player statistics

---
+++ playerstatistics.py
@@ -0,0 +1,27 @@
+name = "Kilian"
+race = "Orc"
+karakterClass = "Warrior"
+gear = "Heavy armor"
+gearStats = 148
+weapon = "Two-handedsword"
+weaponDamage = 48
+level = 49
+carryWeight = 350
+bounty = 0
+guild = "Warriors-guild"
+guildRank = "Leader"
+breatheUnderwater = False 
+
+print(name)
+print(race)
+print(karakterClass)
+print(gear)
+print(gearStats)
+print(weapon)
+print(weaponDamage)
+print(level)
+print(carryWeight)
+print(bounty)
+print(guild)
+print(guildRank)
+print(breatheUnderwater)
