78ba5b48d64e76566a5f41a6b346ec897692174a
Kilian <31303@ma-web.nl>
2020-09-18 12:53:14 +0200

    L2 Player stasts bijgewerkt

--- playerstatistics.py
+++ playerstatistics.py
@@ -3,25 +3,25 @@ race = "Orc"
 karakterClass = "Warrior"
 gear = "Heavy armor"
 gearStats = 148
-weapon = "Two-handedsword"
+weapon = "Two handedsword"
 weaponDamage = 48
 level = 49
 carryWeight = 350
 bounty = 0
-guild = "Warriors-guild"
+guild = "Warriors guild"
 guildRank = "Leader"
 breatheUnderwater = False 
 
-print(name)
-print(race)
-print(karakterClass)
-print(gear)
-print(gearStats)
-print(weapon)
-print(weaponDamage)
-print(level)
-print(carryWeight)
-print(bounty)
-print(guild)
-print(guildRank)
-print(breatheUnderwater)
+print("Name:" + name)
+print("Race:" + race)
+print("Character Class:" + karakterClass)
+print("Gear:" + gear)
+print("Gearstats:" + str(gearStats))
+print("Weapon:" + weapon)
+print("Weapon Damage:" + str(weaponDamage))
+print("Level:" + str(level))
+print("Carry Weight:" + str(carryWeight))
+print("Bounty:" + str(bounty))
+print("Guild:" + guild)
+print("Guild Rank:" + str(guildRank))
+print("Can breathe underwater:" + str(breatheUnderwater))
