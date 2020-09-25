4eee1ed5d245f62b53206c16edd501c6b5f2229a
Kilian <31303@ma-web.nl>
2020-09-25 12:26:37 +0200

    Daily Choices opdracht

---
+++ dailychoices.py
@@ -0,0 +1,54 @@
+print("Welkom ik zou je graag wat vragen willen stellen")
+print("Ik heet Kilian\n")
+jouwNaam = input("Hoe heet jij? ")
+print(jouwNaam + " Welkom bij mijn textbased applicatie ik wil je een paar vragen stellen over daily choices")
+print("Wat drink je als eerste als je net wakker bent, is dat koffie of thee")
+print("a KOFFIE \nb THEE")
+keuze = input("a of b ")
+
+if keuze == "a" or keuze == "A":
+    print("Oeh lekker caffeine")
+elif keuze == "b" or keuze == "B":
+    print("Een lekker rust gevend drankje")
+
+print("Als jij je warme drankje hebt op gedronken ga je daarna eten of wassen")
+print("a ETEN \nb WASSEN")
+keuze2 = input("a of b ")
+
+if keuze2 == "a" or keuze2 == "A":
+    print("Even je maag bijvullen")
+elif keuze2 == "b" or keuze2 == "B":
+    print("Je voelt je vies he ;p")
+
+print("Nu we weten wat je in de ochtend doet. ben ik benieuwd naar wat anders.")
+print("Je gaat naar werk of naar school toe, neem je dan eten vanuit huis mee of koop je het onderweg?")
+print("a ETEN MEENEMEN \nb ETEN KOPEN")
+keuze3 = input("a of b ")
+
+if keuze3 == "a" or keuze3 == "A":
+    print("Gelijk heb je bespaar je tenminste nog geld")
+elif keuze3 == "b" or keuze3 == "B":
+    print("Is wel zonde van je geld maarja het is soms wel lekkerder")
+
+print("Ik ben benieuwd of jij een gestructureerd persoon bent in dagelijkse keuzes")
+print("Laat staan je moet de volgende dag naar school toe. Ga je dan je spullen alvast inpakken of pak je spullen in de ochtend pas in?")
+print("a SPULLEN DAG ERVOOR INPAKKEN \nb SPULLEN OP DE DAG ZELF INPAKKEN")
+keuze4 = input("a of b ")
+
+if keuze4 == "a" or keuze4 == "A":
+    print("Handige keuze scheelt ook weer een hoop voor de volgende dag")
+elif keuze4 == "b" or keuze4 == "B":
+    print("Aah je bent iemand die het op het laatste moment doet beetje stressen in de ochtend ;p")
+
+print("We zijn bijna klaar ik heb namelijk nog één laatste vraag voor jou.")
+print("Ben jij iemand die vroeg gaat slapen en dat je goed uitgerust bent of iemand die laat slaapt en best moe is de volgende dag?")
+print("a VROEG SLAPEN \nb LAAT SLAPEN")
+keuze5 = input("a of b ")
+
+if keuze5 == "a" or keuze5 == "A":
+    print("Goed uitgerust zijn is altijd goed heb je veel meer energie voor die dag")
+elif keuze5 == "b" or keuze5 == "B":
+    print("Naja ik kan je in principe geen ongelijk geven haha")
+
+
+print("Bedankt dat je mee wou doen aan deze vragen!")
