24a507b3394d369806664bb80b666214d7adec8a
Kilian <31303@ma-web.nl>
2020-10-30 12:42:15 +0100

    grocerylist update

--- Grocerieslist.py
+++ Grocerieslist.py
@@ -1,10 +1,10 @@
 grocerylist = ["Milk", "Cheese", "Sausage"]
 shoppingcart = ["Milk", "Cheese", "Sausage"]
 
- 
 
 for x, y in zip(grocerylist, shoppingcart):
-    if x > y:
+    if len(grocerylist) == len(shoppingcart):
+        if x == y:
+            print("Done Shopping")
+    else:
         print("Continue Shopping")
-    if x==y:
-        print("Done Shopping")
